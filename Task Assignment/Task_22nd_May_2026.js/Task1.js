6ba6a9a73d7bb0a12beecc5734a2497b8434b34d
AbhishekBisht24 <abhishekbisht24@example.com>
2026-05-25 01:48:28 +0530

    Add Task Assignment - FizzBuzz and other tasks

---
+++ Task Assignment/Task_22nd_May_2026.js/Task1.js
@@ -0,0 +1,17 @@
+//FizzBuzz Test:-
+
+let a = 7;
+let b = 14;
+let c = 21;
+
+if (a === b && b === c) {
+  console.log("Equilateral");
+}
+
+else if (a === b || b === c || a === c) {
+  console.log("Isosceles");
+}
+
+else {
+  console.log("Scalene");
+}
